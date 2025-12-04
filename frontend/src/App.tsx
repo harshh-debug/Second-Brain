ea71edac57de81bd01f5f290b9ca05ca702c88c9
harshh-debug <harshj9705@gmail.com>
2025-12-05 02:34:23 +0530

    created the button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import Button from './components/ui/Buttons'
+import PlusIcon from './components/ui/icons/PlusIcon'
 
 const App = () => {
   return (
     <>
-   <Button variant='primary' text='first' size='md' onclick={()=>{}}/>
+   <Button variant='primary' text='first' size='sm' onclick={()=>{}} startIcon={<PlusIcon size='md'/>}/>
    <Button variant='secondary' text='second' size='md' onclick={()=>{}}/>
     </>
   )
--- frontend/src/components/ui/Buttons.tsx
+++ frontend/src/components/ui/Buttons.tsx
@@ -10,18 +10,24 @@ interface ButtonProps{
 }
 
 const variantStyles={
-    "primary":"bg-purple-600 text white",
-    "secondary":"bg-purple-400 text-purple-500"
-
+    "primary":"bg-purple-600 text-white",
+    "secondary":"bg-purple-300 text-purple-500"
 }
 
-const defaultStyle="rounded-md p-4"
+const sizeStyles={
+  "sm": "py-1 px-2",
+  "md":"py-2 px-4",
+  "lg":"py-4 px-6"
+}
+
+
+const defaultStyle="rounded-md p-4 flex"
 
 const Button = (props:ButtonProps) => {
   
   return (
-    <button className={variantStyles[props.variant]}>
-        {props.text}
+    <button className={`${variantStyles[props.variant]} ${defaultStyle} ${sizeStyles[props.size]}`}>
+      {props.startIcon}  {props.text} {props.endIcon}
     </button>
   )
 }
